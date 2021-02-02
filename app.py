82c7efa73010eb72f55f7e893f8a37df95b085d4
Amber Teetsel <amberteetsel@Ambers-MacBook-Pro.local>
2021-02-02 12:50:16 -0700

    Initial Flask code setup

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+# Import Dependencies
+## Python Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+## SQLAlchemy Dependencies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+## Flask Dependencies
+from flask import Flask, jsonify
 
-# Create new Flask app instance
+# Set up database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+# Reflect database/tables into classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+# Create variables for each class
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create session link from Python to database
+session = Session(engine)
+
+# Set up Flask
+## Create Flask application 
 app = Flask(__name__)
-
-# Create Flask routes
-## Define root (starting point)
-@app.route('/')
-### Code for specific route goes below root route
-def hello_world():
-    return 'Hello World'
